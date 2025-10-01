5c5d6fecf5ceae37be9441bbde16620adc097532
isura <isura3147@gmail.com>
2025-10-01 18:09:57 +0530

    deleted all customers form. added all customers table to update customer form

--- src/main/java/controller/customerController/UpdateCustomerFormController.java
+++ src/main/java/controller/customerController/UpdateCustomerFormController.java
@@ -1,27 +1,35 @@
 package controller.customerController;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Customer;
 import service.CustomerServiceImpl;
 import service.CustomerService;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 
-public class UpdateCustomerFormController {
+public class UpdateCustomerFormController implements Initializable {
 
-    @FXML
-    public JFXButton btnViewAllCustomers;
     @FXML
     public TextField txtPostalCode;
     CustomerService customerService = new CustomerServiceImpl();
+    ObservableList<Customer> customerInfos = FXCollections.observableArrayList();
     @FXML
     private TextField txtAddress;
     @FXML
@@ -38,7 +46,26 @@ public class UpdateCustomerFormController {
     private TextField txtSalary;
     @FXML
     private TextField txtTitle;
-
+    @FXML
+    private TableView<Customer> tblCustomers;
+    @FXML
+    private TableColumn<?, ?> colAddress;
+    @FXML
+    private TableColumn<?, ?> colCity;
+    @FXML
+    private TableColumn<?, ?> colDOB;
+    @FXML
+    private TableColumn<?, ?> colId;
+    @FXML
+    private TableColumn<?, ?> colName;
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+    @FXML
+    private TableColumn<?, ?> colProvince;
+    @FXML
+    private TableColumn<?, ?> colSalary;
+    @FXML
+    private TableColumn<?, ?> colTitle;
     private Stage stage = new Stage();
     private Stage currentStage;
 
@@ -83,10 +110,6 @@ public class UpdateCustomerFormController {
         nextStage(event, "/view/update_choice_form.fxml");
     }
 
-    @FXML
-    public void btnViewAllCustomersOnAction(ActionEvent event) {
-        nextStage(event, "/view/all_customers_table.fxml");
-    }
 
     private void nextStage(ActionEvent event, String path) {
         currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -97,5 +120,25 @@ public class UpdateCustomerFormController {
         }
         stage.show();
         currentStage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colDOB.setCellValueFactory(new PropertyValueFactory<>("dob"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colCity.setCellValueFactory(new PropertyValueFactory<>("city"));
+        colProvince.setCellValueFactory(new PropertyValueFactory<>("province"));
+        colPostalCode.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
+
+        loadCustomerInfo();
+    }
+
+    private void loadCustomerInfo() {
+        customerInfos.clear();
+        tblCustomers.setItems(customerService.loadDetails(customerInfos));
     }
 }
